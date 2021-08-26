891fe94673eb1b8f018de52ffa5d8a0ca0f7444c
MaxChangInnodisk <max_chang@innodisk.com>
2021-08-26 12:56:18 +0800

    Update Cache

--- tis_tools/log.py
+++ tis_tools/log.py
@@ -2,6 +2,6 @@ import os
 
 def print_title(text):
     # get terminal size
-    columns, rows = os.get_terminal_size(0)
+    columns, rows = os.get_terminal_size()
     print('-'*columns)
     print(text, '\n')
